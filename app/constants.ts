ed5623b4ba2681d20d49185f7db2046d204e9cbb
Peter Trizuliak <peter.trizuliak@gmail.com>
2025-07-14 22:15:27 +0100

    initial basic setup

---
+++ app/constants.ts
@@ -0,0 +1,7 @@
+// TODO: USE YOUR OWN COFFEE URL HERE
+
+// ko-fi.com example
+export const KOFI_URL = "https://ko-fi.com/trizo";
+
+// buymeacoffee.com example
+// export const BUY_ME_A_COFFEE_URL = "https://www.buymeacoffee.com/trizo";
