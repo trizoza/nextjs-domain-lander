599c4e97b85c882d85a02d1efb74b3d1da1d9ee9
Peter Trizuliak <peter.trizuliak@gmail.com>
2025-07-15 21:54:50 +0100

    remove unused font and reword landing

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen pt-0 pb-20 gap-16 sm:p-20">
       <main className="max-w-[360px] flex flex-col gap-[36px] row-start-2 items-center w-full px-3">
         <h1 className="uppercase text-2xl tracking-wider">{host}</h1>
-        <p>What a great domain! We know it too and have grand plans for it. We are working hard to bring it to life soon.</p>
-        <p>If you&apos;d like to <b>learn more</b> or you have your own plans and you&apos;d like to <b>buy {host}</b> from us, <b>get us a coffee</b> to get the conversation started.</p>
+        <p>What a great domain! We have grand plans for it and we are working hard to bring it to life soon.</p>
+        <p>If you&apos;d like to <b>learn more</b> or you have your own plans and you&apos;d like to <b>buy</b> <i>{host}</i> from us, <b>get us a coffee</b> to get the conversation started.</p>
         <a
           className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
           href={KOFI_URL}
