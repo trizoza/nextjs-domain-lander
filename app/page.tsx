24f74961199f1e61e8be12e95c4daf475cec4bce
Peter Trizuliak <peter.trizuliak@gmail.com>
2025-07-15 21:44:57 +0100

    update font and simplify layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,10 @@ export default async function Home() {
   const host = (await headersList).get("host");
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen pt-0 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="max-w-[360px] row-start-1 items-center justify-center">
-        <h1>{host}</h1>
-      </header>
-      <main className="max-w-[360px] flex flex-col gap-[36px] row-start-2 items-start w-full">
-        <p>What a great domain!</p>
-        <p>We know it too and we have great plans for it.</p>
+    <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen pt-0 pb-20 gap-16 sm:p-20">
+      <main className="max-w-[360px] flex flex-col gap-[36px] row-start-2 items-center w-full px-3">
+        <h1 className="uppercase text-2xl tracking-widest">{host}</h1>
+        <p>What a great domain! We know it too and have grand plans for it. We are working hard to bring it to life soon.</p>
         <p>If you&apos;d like to <b>learn more</b> or you have your own plans and you&apos;d like to <b>buy {host}</b> from us, <b>get us a coffee</b> to get the conversation started.</p>
         <a
           className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
@@ -25,7 +22,8 @@ export default async function Home() {
           Buy me a coffee
         </a>
       </main>
-      <footer className="row-start-3 items-center justify-center">
+      <footer className="row-start-3 items-center justify-center text-xs">
+        <p>Made with ❤️ by <a href={KOFI_URL} target="_blank" rel="noopener noreferrer" className="underline">Trizo</a></p>
         <p>Copyright © {new Date().getFullYear()}</p>
       </footer>
     </div>
